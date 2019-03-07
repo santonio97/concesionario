26af809a1c1d099ed7a1455991203487e7c9972f
Jose Antonio Marquez <josemarque97@gmail.com>
2019-03-07 14:33:10 +0100

    archivo config.js

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const config = require('./config');
 
 //importaciones de los modulos
 const app = express(); 
 //conexion a bbdd
-mongoose.connect('mongodb://localhost:27017/autos', { useNewUrlParser: true })
+mongoose.connect(config.db_uri, { useNewUrlParser: true })
     .then(db => console.log('Conexión correcta a la BD'))
     .catch(err => console.log('Error en la conexión a la BD'));
 
@@ -18,4 +19,4 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api', routes);
 // --- PUERTO DE ESCUCHA
-app.listen(3000, () => console.log('Servidor iniciado en puerto 3000'));
+app.listen(config.port, () => console.log('Servidor iniciado en puerto 3000'));
