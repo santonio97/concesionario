9d052403e4d8336cc5a91c7ee72cafedadf0fafb
Jose Antonio Marquez <josemarque97@gmail.com>
2019-03-13 10:49:11 +0100

    redirigir

--- server.js
+++ server.js
@@ -3,21 +3,13 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const routes = require('./routes');
-const config = require('./config');
 
 //importaciones de los modulos
 const app = express(); 
 //conexion a bbdd
-mongoose.connect(config.db_uri, { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost:27017/autos', { useNewUrlParser: true })
     .then(db => console.log('Conexión correcta a la BD'))
     .catch(err => console.log('Error en la conexión a la BD'));
-
-app.use(express.static(path.join(__dirname, 'public')));
-// modulo morgan para logger
-app.use(morgan('dev'));
-// Rutas
-app.use(express.json());
-app.use('/api', routes);
 
 // --- MIDDLEWARE
 // Para redirigir trafico HTTP a HTTPS
@@ -28,5 +20,12 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(express.static(path.join(__dirname, 'public')));
+// modulo morgan para logger
+app.use(morgan('dev'));
+// Rutas
+app.use(express.json());
+app.use('/api', routes);
+
 // --- PUERTO DE ESCUCHA
-app.listen(config.port, () => console.log('Servidor iniciado en puerto 3000'));
+app.listen(3000, () => console.log('Servidor iniciado en puerto 3000'));
