57b5cf300fb25185bd0dbd7d96b0a8c4cd8eeaa1
Santonio <32265704+santonio97@users.noreply.github.com>
2019-03-13 10:44:22 +0100

    redirigir http a https

--- server.js
+++ server.js
@@ -11,13 +11,6 @@ mongoose.connect('mongodb://localhost:27017/autos', { useNewUrlParser: true })
     .then(db => console.log('Conexión correcta a la BD'))
     .catch(err => console.log('Error en la conexión a la BD'));
 
-app.use(express.static(path.join(__dirname, 'public')));
-// modulo morgan para logger
-app.use(morgan('dev'));
-// Rutas
-app.use(express.json());
-app.use('/api', routes);
-
 // --- MIDDLEWARE
 // Para redirigir trafico HTTP a HTTPS
 app.use((req, res, next) => {
@@ -26,6 +19,13 @@ app.use((req, res, next) => {
   else
     next();
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
+// modulo morgan para logger
+app.use(morgan('dev'));
+// Rutas
+app.use(express.json());
+app.use('/api', routes);
 
 // --- PUERTO DE ESCUCHA
 app.listen(3000, () => console.log('Servidor iniciado en puerto 3000'));
